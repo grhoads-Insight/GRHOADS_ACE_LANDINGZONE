15c353ba05df9325a2d9040e2192e1a22fd16577
grhoads-insight <grant.rhoads@insight.com>
2022-08-25 14:21:10 -0400

    fixed subnet error; deploying vnet

--- main.tf
+++ main.tf
@@ -44,19 +44,25 @@ resource "azurerm_virtual_network" "vnet1" {
     location = azurerm_resource_group.rg1.location
     resource_group_name = azurerm_resource_group.rg1.name
     address_space = [ "10.0.0.0/16" ]
+}
 
-    subnet = {
-        name = "appsubnet"
-        address_prefix = "10.0.1.0/24"
-    }
+resource "azurerm_subnet" "appsubnet" {
+    name = "app-subnet"
+    resource_group_name = azurerm_resource_group.rg1.name
+    virtual_network_name = azurerm_virtual_network.vnet1
+    address_prefixes = [ "10.0.1.0/24" ]
+}
 
-    subnet = {
-        name = "datasubnet"
-        address_prefix = "10.0.2.0/24"
-    }
+resource "azurerm_subnet" "datasubnet" {
+    name = "data-subnet"
+    resource_group_name = azurerm_resource_group.rg1.name
+    virtual_network_name = azurerm_virtual_network.vnet1
+    address_prefixes = [ "10.0.1.0/24" ]
+}
 
-    subnet = {
-        name = "websubnet"
-        address_prefix = "10.0.3.0/24"
-    }
+resource "azurerm_subnet" "websubnet" {
+    name = "web-subnet"
+    resource_group_name = azurerm_resource_group.rg1.name
+    virtual_network_name = azurerm_virtual_network.vnet1
+    address_prefixes = [ "10.0.1.0/24" ]
 }
