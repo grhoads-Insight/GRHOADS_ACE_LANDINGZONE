ef61c3cdd3d5c2a7babafc8e873eb0de09c223af
grhoads-insight <grant.rhoads@insight.com>
2022-08-25 19:30:43 -0400

    adding variables for resource names

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "rg2" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "keyvault1" {
-    name = "landing-zone-keyvault"
+    name = var.keyvaultname
     location = azurerm_resource_group.rg1.location
     resource_group_name = azurerm_resource_group.rg1.name
     enabled_for_disk_encryption = true
@@ -40,35 +40,35 @@ resource "azurerm_key_vault" "keyvault1" {
 }
 
 resource "azurerm_virtual_network" "vnet1" {
-    name = "landing-zone-network"
+    name = var.vnetname
     location = azurerm_resource_group.rg1.location
     resource_group_name = azurerm_resource_group.rg1.name
     address_space = [ "10.0.0.0/16" ]
 }
 
 resource "azurerm_subnet" "appsubnet" {
-    name = "app-subnet"
+    name = var.subnet1name
     resource_group_name = azurerm_resource_group.rg1.name
     virtual_network_name = azurerm_virtual_network.vnet1.name
     address_prefixes = [ "10.0.1.0/24" ]
 }
 
 resource "azurerm_subnet" "datasubnet" {
-    name = "data-subnet"
+    name = var.subnet2name
     resource_group_name = azurerm_resource_group.rg1.name
     virtual_network_name = azurerm_virtual_network.vnet1.name
     address_prefixes = [ "10.0.2.0/24" ]
 }
 
 resource "azurerm_subnet" "websubnet" {
-    name = "web-subnet"
+    name = var.subnet3name
     resource_group_name = azurerm_resource_group.rg1.name
     virtual_network_name = azurerm_virtual_network.vnet1.name
     address_prefixes = [ "10.0.3.0/24" ]
 }
 
 resource "azurerm_storage_account" "storage1" {
-    name = "grhoadsacelzstorage"
+    name = var.storage_account_name
     location = azurerm_resource_group.rg1.location
     resource_group_name = azurerm_resource_group.rg1.name
     account_tier = "Standard"
@@ -77,13 +77,13 @@ resource "azurerm_storage_account" "storage1" {
 }
 
 resource "azurerm_storage_container" "storagecontainer1" {
-    name = "grhoadsacelzcontainer"
+    name = var.storage_container_name
     storage_account_name = azurerm_storage_account.storage1.name
     container_access_type = "private"
 }
 
 resource "azurerm_storage_blob" "storageblob1" {
-    name = "grhoadsacelzblob"
+    name = var.blob_storage_name
     storage_account_name = azurerm_storage_account.storage1.name
     storage_container_name = azurerm_storage_container.storagecontainer1.name
     type = "Block"
--- variables.tf
+++ variables.tf
@@ -32,3 +32,51 @@ variable "tags" {
     type = map(string)
     default = {}
 }
+
+variable "keyvaultname" {
+    description = "name of keyvault you would like, only numbers and dashes"
+    type = string
+    default = "landing-zone-keyvault"
+}
+
+variable "vnetname" {
+    description = "name of virtual network"
+    type = string
+    defualt = "landing-zone-network"
+}
+
+variable "subnet1name" {
+    description = "name of first subnet for app"
+    type = string
+    default = "app-subnet"
+}
+
+variable "subnet2name" {
+    description = "name of first subnet for data"
+    type = string
+    default = "data-subnet"
+}
+
+variable "subnet3name" {
+    description = "name of first subnet for web"
+    type = string
+    default = "web-subnet"
+}
+
+variable "storage_account_name" {
+    description = "name for the storage account in landing zone"
+    type = string
+    default = "grhoadsacelzstorage"
+}
+
+variable "storage_container_name" {
+    description = "name for storage container"
+    type = string
+    default = "grhoadsacelzcontainer"
+}
+
+variable "blob_storage_name" {
+    description = "name for blob inside container"
+    type = string
+    default = "grhoadsacelzblob"
+}
