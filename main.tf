339b815d6be6192350edd20b8e1eac274133f387
grhoads-insight <grant.rhoads@insight.com>
2022-08-25 13:14:19 -0400

    fixed typo in main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_key_vault" "keyvault1" {
     name = "landing_zone_keyvault"
     location = azurerm_resource_group.rg1.location
     resource_group_name = azurerm_resource_group.rg1.name
-    enabled_for_disk_encryption = 
+    enabled_for_disk_encryption = true
     tenant_id = data.azurerm_client_config.current.tenant_id
     soft_delete_retention_days = 7
     purge_protection_enabled = false
