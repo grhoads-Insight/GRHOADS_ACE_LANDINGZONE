f6ab961ef1613c2d24c5c9adf180fee184252579
grhoads-insight <grant.rhoads@insight.com>
2022-08-25 12:31:19 -0400

    updated main.tf by removing a module

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-module "service_principal" {
-    source  = "app.terraform.io/insight/terraform-app-registration/azuread"
-    version = "~> 2.5.0"
-
-    app_registration_name = "grhoads-terraform-ace-2022"
-}
-
 resource "azurerm_resource_group" "rg1" {
     name = var.rg1
     location = var.location
