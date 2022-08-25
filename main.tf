e22c55029bdb5598ac4f513a3607f4c7833cd201
grhoads-insight <grant.rhoads@insight.com>
2022-08-25 14:16:56 -0400

    adding virtual network resource to landing zone

--- main.tf
+++ main.tf
@@ -38,3 +38,25 @@ resource "azurerm_key_vault" "keyvault1" {
         ]
     }
 }
+
+resource "azurerm_virtual_network" "vnet1" {
+    name = "landing-zone-network"
+    location = azurerm_resource_group.rg1.location
+    resource_group_name = azurerm_resource_group.rg1.name
+    address_space = [ "10.0.0.0/16" ]
+
+    subnet = {
+        name = "appsubnet"
+        address_prefix = "10.0.1.0/24"
+    }
+
+    subnet = {
+        name = "datasubnet"
+        address_prefix = "10.0.2.0/24"
+    }
+
+    subnet = {
+        name = "websubnet"
+        address_prefix = "10.0.3.0/24"
+    }
+}
