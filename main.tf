d3b128d93c3a1e0bef76cecfc2343c3de7e5777a
grhoads-insight <grant.rhoads@insight.com>
2022-08-25 14:29:05 -0400

    fixing address pool for subnets

--- main.tf
+++ main.tf
@@ -57,12 +57,12 @@ resource "azurerm_subnet" "datasubnet" {
     name = "data-subnet"
     resource_group_name = azurerm_resource_group.rg1.name
     virtual_network_name = azurerm_virtual_network.vnet1.name
-    address_prefixes = [ "10.0.1.0/24" ]
+    address_prefixes = [ "10.0.2.0/24" ]
 }
 
 resource "azurerm_subnet" "websubnet" {
     name = "web-subnet"
     resource_group_name = azurerm_resource_group.rg1.name
     virtual_network_name = azurerm_virtual_network.vnet1.name
-    address_prefixes = [ "10.0.1.0/24" ]
+    address_prefixes = [ "10.0.3.0/24" ]
 }
