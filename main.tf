16ca3a29773b7463d81727636a1c42d224863110
grhoads-insight <grant.rhoads@insight.com>
2022-08-31 21:00:22 -0400

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,114 +1,114 @@
 resource "azurerm_resource_group" "rg1" {
-    name = var.rg1
-    location = var.location
+  name     = var.rg1
+  location = var.location
 }
 
 resource "azurerm_resource_group" "rg2" {
-    name = var.rg2
-    location = var.location
+  name     = var.rg2
+  location = var.location
 }
 
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "keyvault1" {
-    name = var.keyvaultname
-    location = azurerm_resource_group.rg1.location
-    resource_group_name = azurerm_resource_group.rg1.name
-    enabled_for_disk_encryption = true
+  name                        = var.keyvaultname
+  location                    = azurerm_resource_group.rg1.location
+  resource_group_name         = azurerm_resource_group.rg1.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
-    soft_delete_retention_days = 7
-    purge_protection_enabled = false
+    object_id = data.azurerm_client_config.current.object_id
 
-    sku_name = "standard"
+    key_permissions = [
+      "Get",
+    ]
 
-    access_policy {
-        tenant_id = data.azurerm_client_config.current.tenant_id
-        object_id = data.azurerm_client_config.current.object_id
+    secret_permissions = [
+      "Get",
+    ]
 
-        key_permissions = [
-            "Get",
-        ]
-
-        secret_permissions = [
-            "Get",
-        ]
-
-        storage_permissions = [
-            "Get",
-        ]
-    }
+    storage_permissions = [
+      "Get",
+    ]
+  }
 }
 resource "azurerm_virtual_network" "vnet1" {
-    name = var.vnetname
-    location = azurerm_resource_group.rg1.location
-    resource_group_name = azurerm_resource_group.rg1.name
-    address_space = [ "10.0.0.0/16" ]
+  name                = var.vnetname
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "appsubnet" {
-    name = var.subnet1name
-    resource_group_name = azurerm_resource_group.rg1.name
-    virtual_network_name = azurerm_virtual_network.vnet1.name
-    address_prefixes = [ "10.0.1.0/24" ]
+  name                 = var.subnet1name
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.0.1.0/24"]
 
-    delegation {
-      name = "appsubnet-delegation"
+  delegation {
+    name = "appsubnet-delegation"
 
-      service_delegation {
-        name = "Microsoft.Web/serverFarms"
-        actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-      }
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
     }
+  }
 }
 
 resource "azurerm_subnet" "datasubnet" {
-    name = var.subnet2name
-    resource_group_name = azurerm_resource_group.rg1.name
-    virtual_network_name = azurerm_virtual_network.vnet1.name
-    address_prefixes = [ "10.0.2.0/24" ]
-    delegation {
-      name = "appsubnet-delegation"
+  name                 = var.subnet2name
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.0.2.0/24"]
+  delegation {
+    name = "appsubnet-delegation"
 
-      service_delegation {
-        name = "Microsoft.Web/serverFarms"
-        actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-      }
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
     }
+  }
 }
 
 resource "azurerm_subnet" "websubnet" {
-    name = var.subnet3name
-    resource_group_name = azurerm_resource_group.rg1.name
-    virtual_network_name = azurerm_virtual_network.vnet1.name
-    address_prefixes = [ "10.0.3.0/24" ]
-    delegation {
-      name = "appsubnet-delegation"
+  name                 = var.subnet3name
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.0.3.0/24"]
+  delegation {
+    name = "appsubnet-delegation"
 
-      service_delegation {
-        name = "Microsoft.Web/serverFarms"
-        actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-      }
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
     }
+  }
 }
 
 resource "azurerm_storage_account" "storage1" {
-    name = var.storage_account_name
-    location = azurerm_resource_group.rg1.location
-    resource_group_name = azurerm_resource_group.rg1.name
-    account_tier = "Standard"
-    account_replication_type = "GRS"
-    account_kind = "StorageV2"
+  name                     = var.storage_account_name
+  location                 = azurerm_resource_group.rg1.location
+  resource_group_name      = azurerm_resource_group.rg1.name
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  account_kind             = "StorageV2"
 }
 
 resource "azurerm_storage_container" "storagecontainer1" {
-    name = var.storage_container_name
-    storage_account_name = azurerm_storage_account.storage1.name
-    container_access_type = "private"
+  name                  = var.storage_container_name
+  storage_account_name  = azurerm_storage_account.storage1.name
+  container_access_type = "private"
 }
 
 resource "azurerm_storage_blob" "storageblob1" {
-    name = var.blob_storage_name
-    storage_account_name = azurerm_storage_account.storage1.name
-    storage_container_name = azurerm_storage_container.storagecontainer1.name
-    type = "Block"
+  name                   = var.blob_storage_name
+  storage_account_name   = azurerm_storage_account.storage1.name
+  storage_container_name = azurerm_storage_container.storagecontainer1.name
+  type                   = "Block"
 }
--- variables.tf
+++ variables.tf
@@ -1,89 +1,89 @@
 variable "name" {
-    description = "The Landing Zone name"
-    type = string
-    default = "grhoads_capstone_lz"
+  description = "The Landing Zone name"
+  type        = string
+  default     = "grhoads_capstone_lz"
 }
 
 variable "env" {
-    description = "environment name"
-    type = string
-    default = "dev"
+  description = "environment name"
+  type        = string
+  default     = "dev"
 }
 
 variable "location" {
-    description = "The location zone for Landing Zone resources"
-    type = string
-    default = "eastus2" #centralus redundant partner region
+  description = "The location zone for Landing Zone resources"
+  type        = string
+  default     = "eastus2" #centralus redundant partner region
 }
 
 variable "location2" {
-    description = "backup/redundant region for Landing Zone Resources"
-    type = string
-    default = "centralus"
+  description = "backup/redundant region for Landing Zone Resources"
+  type        = string
+  default     = "centralus"
 }
 
 variable "rg1" {
-    description = "The name of the landing zone resource group"
-    type = string
-    default = "landing_zone_rg"
+  description = "The name of the landing zone resource group"
+  type        = string
+  default     = "landing_zone_rg"
 }
 
 variable "rg2" {
-    description = "The name of the workload resource group"
-    type = string
-    default = "workload_rg"
+  description = "The name of the workload resource group"
+  type        = string
+  default     = "workload_rg"
 }
 
 variable "tags" {
-    description = "Additional tags to apply to resources"
-    type = map(string)
-    default = {}
+  description = "Additional tags to apply to resources"
+  type        = map(string)
+  default     = {}
 }
 
 variable "keyvaultname" {
-    description = "name of keyvault you would like, only numbers and dashes"
-    type = string
-    default = "landing-zone-keyvault"
+  description = "name of keyvault you would like, only numbers and dashes"
+  type        = string
+  default     = "landing-zone-keyvault"
 }
 
 variable "vnetname" {
-    description = "name of virtual network"
-    type = string
-    default = "landing-zone-network"
+  description = "name of virtual network"
+  type        = string
+  default     = "landing-zone-network"
 }
 
 variable "subnet1name" {
-    description = "name of first subnet for app"
-    type = string
-    default = "app-subnet"
+  description = "name of first subnet for app"
+  type        = string
+  default     = "app-subnet"
 }
 
 variable "subnet2name" {
-    description = "name of first subnet for data"
-    type = string
-    default = "data-subnet"
+  description = "name of first subnet for data"
+  type        = string
+  default     = "data-subnet"
 }
 
 variable "subnet3name" {
-    description = "name of first subnet for web"
-    type = string
-    default = "web-subnet"
+  description = "name of first subnet for web"
+  type        = string
+  default     = "web-subnet"
 }
 
 variable "storage_account_name" {
-    description = "name for the storage account in landing zone"
-    type = string
-    default = "grhoadsacelzstorage"
+  description = "name for the storage account in landing zone"
+  type        = string
+  default     = "grhoadsacelzstorage"
 }
 
 variable "storage_container_name" {
-    description = "name for storage container"
-    type = string
-    default = "grhoadsacelzcontainer"
+  description = "name for storage container"
+  type        = string
+  default     = "grhoadsacelzcontainer"
 }
 
 variable "blob_storage_name" {
-    description = "name for blob inside container"
-    type = string
-    default = "grhoadsacelzblob"
+  description = "name for blob inside container"
+  type        = string
+  default     = "grhoadsacelzblob"
 }
